98279ef37eff0b7dcaf8384ea3e1d5fa1df47bd3
CenTdemeern1 <timo.herngreen@gmail.com>
2026-06-11 18:48:40 +0200

    Add code comments explaining ExampleMod
    Figured this might be useful since without these comments you're just injecting random bytes in random places and it's not particularly self-documenting

--- ExampleMod/dllmain.cpp
+++ ExampleMod/dllmain.cpp
@@ -5,54 +5,33 @@
 #include <Extender/util.h>
 
 void onPreInit(Sexy::WinTwistApp* app) {
-    printf_s("Hello from a C++ native mod's preInit hook!");
+    printf_s("Hello world from ExampleMod's preInit hook!");
 }
-
-void evilPreInit(Sexy::WinTwistApp* app) {
-    printf_s("Hello! I am evil!");
-}
-
-void onBoardUpdate(TwistFunctions* game) {
-    printf_s("The board has updated! Let's twist\n");
-    //for (int y = 0; y < 7; y++) {
-    //    for (int x = 0; x < 7; x++) {
-    //        game->TwistGemsAt(false, x, y);
-    //    }
-    //}
-}
-
-//void __declspec(naked) levelUpMeterHoverInjection() {
-//    __asm
-//    {
-//
-//    }
-//}
 
 extern "C" bool __declspec(dllexport) __cdecl initMod(CodeInjection::FuncInterceptor* interceptor, HookFunctions* hooks)
 {
-    //interceptor->intercept(onPreInit, evilPreInit);
-    //onPreInit(nullptr);
+    hooks->registerPreInitHook(onPreInit);
 
     // This is in the middle of an instruction(!!)
     // This patches the instruction at 0x6d5a67 to load the levelup requirement instead of the current level
-    // Offset 0x204 in GameManager seems to be the levelup requirement
+    // Offset 0x204 in GameManager is the levelup requirement
     inject_byte(0x6d5a69, 0x04);
     inject_byte(0x6d5a6A, 0x02);
-    // Also useful to know: offset 0x200 in GameManager seems to be the progress towards the levelup requirement
 
+    // Remove the math operations being applied to the current level progress value
     inject_nops(0x6D5A71, 12);
 
-    //constexpr const char string[] = "%d%% of level %d or something idk\0";
-    //                              "You have completed %d%% of Level %d";
-    constexpr const char string[] = "You have scored %d out of %d points";
-    for (int i = 0; string[i]; i++) {
+    // Replace the tooltip text. This string is the same length as the original!
+    //                                   "You have completed %d%% of Level %d"
+    constexpr const char tooltipText[] = "You have scored %d out of %d points";
+    for (int i = 0; tooltipText[i]; i++) {
         // 8-bit version
-        inject_byte(0x7deac0, string[i]);
+        inject_byte(0x7deac0, tooltipText[i]);
         // Wide version
-        inject_byte(0x7dea78 + i * 2, string[i]);
+        inject_byte(0x7dea78 + i * 2, tooltipText[i]);
         inject_byte(0x7dea79 + i * 2, 0);
     }
-    hooks->registerPreInitHook(onPreInit);
-    hooks->registerUpdateBoardHook(onBoardUpdate);
+
+    printf_s("ExampleMod has been initialized successfully!\n");
     return true;
 }
